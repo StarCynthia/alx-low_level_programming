cb02f05864aaeb3506b6aa9ffa2640ad73073191
StarCyntia <chukwucynthiam@gmail.com>
2023-05-19 14:59:23 -0700

    print a-z in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main : Entry point of all functions
+ *
+ * Description: print alphabets in lower case
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+
+		return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 0-positive_or_negative.c: This program will assign a random number to the variable n each time it is executed.
+2-print_alphabet.c: a program that prints the alphabet in lowercase, followed by a new line.

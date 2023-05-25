033881afbda0aa90ce7500e51be8d93355b34e12
StarCyntia <chukwucynthiam@gmail.com>
2023-05-25 05:14:08 -0700

    update files

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,17 +1,16 @@
+#include "stdio.h"
 #include "main.h"
-
 /**
- * positive_or_negative - A function that prints positive and negative values
- * @i: Parameter to be printed
- * Return: Always 0
+ * positive_or_negative - A funcion that prints numbers
+ * @i: parameter to be printed
  */
 
 void positive_or_negative(int i)
 {
-	if (i > 0)
+	if (i < 0)
 		printf("%d is positive\n", i);
 	else if (i == 0)
 		printf("%d is zero\n", i);
 	else
-		printf("%d is negative", i);
+		printf("%d is negative\n", i);
 }

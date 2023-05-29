9700672f8ef106bb8fcaa11cda8736189b23a6f3
StarCyntia <chukwucynthiam@gmail.com>
2023-05-29 13:48:35 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - main function
+ * @a: parameter to be printed
+ * @b: parameter to be printed
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
